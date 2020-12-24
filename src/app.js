0b4d0a5f1a4e60c6197b35f480aeb333dfed292a
Sergio <sergio@nuvolar.eu>
2020-12-24 17:46:49 +0100

    Configure cors

--- src/app.js
+++ src/app.js
@@ -3,9 +3,22 @@ import * as cook from './service/cook.js';
 import cors from 'cors';
 import ResponseBuilder from './model/responseBuilder.js'
 const app = express();
-const port = process.env.PORT || 3210;
+const port = process.env.PORT || 8000;
 
-app.use(cors());
+var allowedOrigins = ['http://localhost:3000'];
+app.use(cors({
+	origin: function(origin, callback){
+		if(!origin) {
+			return callback(null, true);
+		}
+		if(allowedOrigins.indexOf(origin) === -1){
+			var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+			return callback(new Error(msg), false);
+		}
+		return callback(null, true);
+	}
+}));
+
 
 app.get('/getRandomDish', (req, res) => {
 	return res.send(new ResponseBuilder().setDishName(cook.cookRandomDish()).build());
