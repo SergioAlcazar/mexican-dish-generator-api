364265cbe218f9f9ec7b78c7c90bc59e987fc9d7
Sergio <sergio@nuvolar.eu>
2020-12-24 17:16:31 +0100

    Add cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import * as cook from './service/cook.js';
+import cors from 'cors';
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.get('/getRandomDish', (req, res) => {
 	return res.send(cook.cookRandomDish());
